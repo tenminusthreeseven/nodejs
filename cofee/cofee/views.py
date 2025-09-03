8018a62ff031b042df769391f11e59047248b83d
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-03 19:16:54 +0530

    ye

--- cofee/cofee/views.py
+++ cofee/cofee/views.py
@@ -2,13 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
   return render(request,'index.html')
-def home(request):
-  return HttpResponse("hello world ,you are nice")
 
-def about(request):
-  return HttpResponse("you dumbfuck")
-
-def contact(request):
-  return("you dumnbfuck")
 
 
