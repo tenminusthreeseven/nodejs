8a6d18d169aa530b459d5eee26ba3acd2f30cd3d
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-03 18:12:42 +0530

    ye

--- cofee/cofee/views.py
+++ cofee/cofee/views.py
@@ -1,1 +1,10 @@
 from django.http import HttpResponse
+def home(request):
+  return HttpResponse("hello world ,you are nice")
+
+def about(request):
+  return HttpResponse("you dumbfuck")
+
+def contact(request):
+  return("you dumnbfuck")
+
