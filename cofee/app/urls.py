81eda86a18b7ca56fa91478b9307a107f121cc0a
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-05 17:12:13 +0530

    t

---
+++ cofee/app/urls.py
@@ -0,0 +1,7 @@
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+
+]
--- cofee/cofee/views.py
+++ cofee/cofee/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
   return render(request,'index.html')
+def all_chai(request):
+  return render(request,'chai/all_chai.html')
 
 
 
