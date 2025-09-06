a1e6f729f6c7a5d43d090455dd5e11801945f3c6
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-06 11:31:28 +0530

    ye

--- cofee/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return HttpResponse("Welcome to the Coffee app!")
-
-def all_chai(request):
-    return render(request, 'all_chai.html')
--- cofee/cofee/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def all_chai(request):
-  return render(request,'app/all_chai.html')
-
-
-
