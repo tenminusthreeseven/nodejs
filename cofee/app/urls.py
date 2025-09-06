0a6e25d97cf5ff3b19d03ac5874f68f4b37b2f8f
Adarsh Kumar <adarshkumar12103@gmail.com>
2025-09-06 10:26:39 +0530

    ye

--- cofee/app/urls.py
+++ cofee/app/urls.py
@@ -3,5 +3,5 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
-
+path('contact/',views.contact,name='contact'),
 ]
